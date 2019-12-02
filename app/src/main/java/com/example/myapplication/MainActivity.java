d4a5e43a37f52ec68c098fab3a883fc8cde816aa
Kevin li <kevinli@illinois.edu>
2019-12-02 15:49:55 -0600

    Finished UI part 1.1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button startGame = findViewById(R.id.start);
         Button leaderBoard = findViewById(R.id.leaderboard);
+        Button logOut = findViewById(R.id.logout);
         startGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Leaderboard.class));
+            }
+        });
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth logBruh = FirebaseAuth.getInstance();
+                logBruh.signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
             }
         });
     }
