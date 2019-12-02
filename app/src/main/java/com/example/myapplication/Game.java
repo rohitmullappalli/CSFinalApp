d1795351d0d1590772e647cdaadeb22d3af41dba
Kevin li <kevinli@illinois.edu>
2019-12-02 15:09:40 -0600

    Finished UI part 1

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -1,9 +1,4 @@
 package com.example.myapplication;
 
 public class Game {
-    private int[][] gameBoard;
-    private int[] position;
-    public int[] move(int[] position) {
-
-    }
 }
---
+++ app/src/main/java/com/example/myapplication/Leaderboard.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Leaderboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+        Button back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+    }
+}
