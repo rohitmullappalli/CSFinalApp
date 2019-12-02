e37504856d68828aed9c4d37601c360c8142ff0f
Kevin li <kevinli@illinois.edu>
2019-12-02 14:30:09 -0600

    Merge branch 'master' of https://github.com/rohitmullappalli/CSFinalApp
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -1,8 +1,21 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +23,37 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText lEmail = findViewById(R.id.email2);
+        final EditText lPassword = findViewById(R.id.password2);
+        Button login = findViewById(R.id.loginbutton);
+        Button registerInstead = findViewById(R.id.registerinstead);
+        final FirebaseAuth lAuth = FirebaseAuth.getInstance();
+
+        registerInstead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = lEmail.getText().toString().trim();
+                String password = lPassword.getText().toString().trim();
+                lAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Login.this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -1,11 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
@@ -18,6 +27,19 @@ public class Register extends AppCompatActivity {
         final EditText ePassword = findViewById(R.id.password);
         Button register = findViewById(R.id.register);
         Button loginInstead = findViewById(R.id.logininstead);
+        final FirebaseAuth fAuth = FirebaseAuth.getInstance();
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity (new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+        loginInstead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +47,30 @@ public class Register extends AppCompatActivity {
                 String email = eEmail.getText().toString().trim();
                 String name = eName.getText().toString().trim();
                 String password = ePassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email)) {
+                    eEmail.setError("Must have an Email");
+                }
+                if(TextUtils.isEmpty(password)) {
+                    ePassword.setError("Must have a password");
+                }
+                if(TextUtils.isEmpty(name)) {
+                    eName.setError("At least one character is required in Name");
+                }
+                if(password.length() < 5) {
+                    ePassword.setError("Password must be at least 5 characters");
+                }
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Register.this, "New User Made", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
             }
         });
 
