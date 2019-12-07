5c7f1bb3a9f230130dd899c9931b1f2ba6dccac6
rohitmullappalli <rohitm4@illinois.edu>
2019-12-06 18:18:01 -0600

    commit

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -1,4 +1,20 @@
 package com.example.myapplication;
 
-public class Game {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Game extends AppCompatActivity {
+    private int xPosition;
+    private int yPosition;
+    private int boardSize;
+    Game(int x, int y, int b) {
+        xPosition = x;
+        yPosition = y;
+        boardSize = b;
+    }
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
