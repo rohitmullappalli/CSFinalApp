334a1422c371ca541529f21d73fe1e4c27036aa6
rohitmullappalli <rohitm4@illinois.edu>
2019-12-08 17:07:23 -0600

    random solutions implemented

--- app/src/main/java/com/example/myapplication/GamePanel.java
+++ app/src/main/java/com/example/myapplication/GamePanel.java
@@ -192,4 +192,5 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
 
 
+
 }
--- app/src/main/java/com/example/myapplication/Solutions.java
+++ app/src/main/java/com/example/myapplication/Solutions.java
@@ -5,42 +5,20 @@ import java.util.List;
 import java.util.Random;
 
 public class Solutions {
-    private List<Integer> listXOdd = new ArrayList<>();
-    private List<Integer> listYOdd = new ArrayList<>();
-    private List<Integer> listXEven = new ArrayList<>();
-    private List<Integer> listYEven = new ArrayList<>();
+    private List<int[]> list = new ArrayList<>();
+    private int gridSize;
+    private int[] currentPosition = new int[2];
+    public void move(int[] current) {
+        current = currentPosition;
+        if (current[0]  + 1 > gridSize || current[0] - 1 < 0) {
+            return;
+        }
+        if (current[1]  + 1 > gridSize || current[1] - 1< 0) {
+            return;
+        }
+        if (list.contains(current)) {
+            return;
+        }
 
-    public void possibleSolutions(int gridSize) {
-        for (int i = 0; i < gridSize; i = i + 2) {
-            listXEven.add(i);
-            listYEven.add(i);
-        }
-        for (int k = 1; k < gridSize; k = k + 2) {
-            listXOdd.add(k);
-            listYOdd.add(k);
-        }
-    }
-
-    public int[] randomSolution() {
-        int[] a = new int[] { 1, 2}; //1 is odds, 2 is evens
-        int[] solution = new int[2];
-        double b = Math.floor(Math.random() * a.length);
-        int c = (int) b;
-        int d = a[c];
-        if (d == 1) {
-            Random rand = new Random();
-            int randomX = listXOdd.get(rand.nextInt(listXOdd.size()));
-            int randomY = listYOdd.get(rand.nextInt(listYOdd.size()));
-            solution[0] = randomX;
-            solution[1] = randomY;
-            return solution;
-        } else {
-            Random random = new Random();
-            int randomX = listXEven.get(random.nextInt(listXOdd.size()));
-            int randomY = listYEven.get(random.nextInt(listYOdd.size()));
-            solution[0] = randomX;
-            solution[1] = randomY;
-            return solution;
-        }
     }
 }
